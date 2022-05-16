0f7069743d535b48d41edf53f49dc41011266d84
tudao <nevarda9416@gmail.com>
2022-05-16 23:30:58 +0700

    - Template frontend and backend (2) - Route, folder frontend and backend

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,38 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+// Frontend
+Route::group(['middleware' => 'web', 'namespace' => 'Frontend'], function () {
+    Route::get('/', ['as' => 'homepage', 'uses' => 'HomepageController@index']);
+    Route::get('/gioi-thieu', ['as' => 'aboutus', 'uses' => 'AboutusController@index']);
+    Route::post('/customer-register', ['middleware' => 'throttle:2000,1', 'uses' => 'CustomerController@register']);
 });
 
-Route::get('/vrp_admin', function () {
-    return view('vrp_admin/index');
+// Backend
+// Authentication Routes...
+Route::get('login', ['as' => 'login', 'uses' => 'Auth\LoginController@getLogin']);
+Route::post('login', ['uses' => 'Auth\LoginController@postLogin']);
+Route::get('logout', ['as' => 'logout', 'uses' => 'Auth\LoginController@logout']);
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/vrp_backend', 'DashboardController@index');
+
+    // Đường dẫn allow upload ảnh từ trong ckeditor
+    Route::post('ckeditor/image_upload', 'CKEditorController@upload')->name('upload');
+
+    // Trang quản trị user
+    Route::group(['prefix' => 'users', 'as' => 'user', 'middleware' => 'auth'], function () {
+        Route::get('/', ['as' => 'index', 'uses' => 'UserController@index']);
+        Route::get('/create', ['uses' => 'UserController@create']);
+        Route::post('/store', ['uses' => 'UserController@store']);
+        Route::get('/show/{id?}', ['uses' => 'UserController@show']);
+        Route::get('/edit/{id?}', ['uses' => 'UserController@edit']);
+        Route::post('/update/{id?}', ['uses' => 'UserController@update']);
+        Route::get('/delete/{id?}', ['uses' => 'UserController@destroy']);
+    });
+
+    // Quản lý người dùng đăng ký
+    Route::group(['prefix' => 'customer'], function () {
+        Route::get('/', ['as' => 'index', 'uses' => 'CustomerController@index']);
+    });
 });
